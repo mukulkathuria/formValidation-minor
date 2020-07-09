2b9e31894ade6bf449552e49bb0bbbb94ef2ac68
Mukul Kathuria <mukulkathuria008@yahoo.com>
2020-07-09 15:18:10 +0530

    Session Storage on ContextAPI

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -6,12 +6,17 @@ export const Authentication = props => {
         const [auth,changeauth] = React.useState({
             data:null
         });
+        
         React.useEffect(()=>{
             if(auth.data){
-                sessionStorage.setItem('auth',JSON.stringify(auth.data))
+                sessionStorage.setItem('auth',JSON.stringify(auth.data));
             }
         },[auth.data])
-        
+        const authValue=sessionStorage.getItem('auth');
+
+        if(!auth.data && authValue){
+            changeauth({data:authValue});
+        }
     return(
             <Auth.Provider value={[auth,changeauth]}>
                 {props.children}
